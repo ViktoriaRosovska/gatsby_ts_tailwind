6c4737e8b7b2d6d4b327bec3c082fe6dc90025ee
Viktoria Rosovska <torimeister@gmail.com>
2024-09-20 01:09:15 +0300

    push updates

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,6 +1,7 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
+  pathPrefix: "/gatsby_ts_tailwind",
   siteMetadata: {
     title: `gatsby_ts_tailwind`,
     siteUrl: `https://www.yourdomain.tld`,
